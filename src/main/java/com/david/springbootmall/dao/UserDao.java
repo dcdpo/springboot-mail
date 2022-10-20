4f9389e69d88d19cc0bb454fca44ae59d12647e4
David <sodagreenapay@gmail.com>
2022-10-20 17:46:18 +0800

    帳號功能 - 檢查註冊的 email|、隱藏返回的密碼

--- src/main/java/com/david/springbootmall/dao/UserDao.java
+++ src/main/java/com/david/springbootmall/dao/UserDao.java
@@ -2,10 +2,13 @@ package com.david.springbootmall.dao;
 
 import com.david.springbootmall.dto.UserRegisterRequest;
 import com.david.springbootmall.model.User;
+import org.springframework.stereotype.Component;
+
 
 public interface UserDao {
-
     User getUserById(Integer userId);
+
+    User getUserByEmail(String email);
     Integer createUser(UserRegisterRequest userRegisterRequest);
 
 }
